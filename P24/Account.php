0c40db569c744862628abd315bf77d984185d1a3
halt <halt.avmc@gmail.com>
2016-08-04 12:13:18 +0300

    Add some explanations about "balance" and "info" arrays

--- P24/Account.php
+++ P24/Account.php
@@ -19,24 +19,24 @@ class Account
   protected $balance_uri = "https://api.privatbank.ua/p24api/balance";
 
   protected $balance = [
-    'av_balance'=>null,
-    'bal_date'=>null,
-    'bal_dyn'=>null,
-    'balance'=>null,
-    'fin_limit'=>null,
-    'trade_limit'=>null,
+    'av_balance'=>null,   // Доступные средства. Это средства, которыми можно оперировать
+    'bal_date'=>null,     // Дата баланса
+    'bal_dyn'=>null,      // ??
+    'balance'=>null,      // Полный баланс. Сюда входят, в т.ч. средства, заблокированные на карте (HOLD ?)
+    'fin_limit'=>null,    // Кредитный лимит. Например на кредитной карте
+    'trade_limit'=>null,  // ??
   ];
-  
+
   protected $info = [
-    'account'=>null,
-    'card_number'=>null,
-    'acc_name'=>null,
-    'acc_type'=>null,
-    'currency'=>null,
-    'card_type'=>null,
-    'main_card_number'=>null,
-    'card_stat'=>null,
-    'src'=>null,
+    'account'=>null,          // Счет карты
+    'acc_name'=>null,         // Название счёта ("Виртуальный счет Приват24")
+    'acc_type'=>null,         // Тип счёта  (CM, CC, ??)
+    'card_number'=>null,      // Номер карты
+    'main_card_number'=>null, // Номер основной карты
+    'card_type'=>null,        // Тип карты ("Карта для выплат")
+    'currency'=>null,         // Валюта
+    'card_stat'=>null,        // Статус карты (NORM - всё ОК, RSTR - заблокирована, ??)
+    'src'=>null,              // ?? (M)
   ];
 
   public function __construct($merchant, $account = null)
@@ -55,54 +55,52 @@ class Account
     $wait = $this->merchant->wait();
     $test = $this->merchant->test();
     $oper = "cmt";
-    
+
     $req_string = "<request />";
     $request = new SimpleXMLElement($req_string);
-	$request->addAttribute("version", "1.0");
-    
+	     $request->addAttribute("version", "1.0");
+
     $merchant = $request->addChild("merchant");
-	$merchant->addChild("id", $id);
-    
-    $data = $request->addChild("data");    
-	$data->addChild("oper", $oper);
-	$data->addChild("wait", $wait);
-	$data->addChild("test", $test);
-    
+	     $merchant->addChild("id", $id);
+
+    $data = $request->addChild("data");
+    	$data->addChild("oper", $oper);
+    	$data->addChild("wait", $wait);
+    	$data->addChild("test", $test);
+
     $payment = $data->addChild("payment");
-    
+
     if (isset($this->info['account']))
     {
       $acc = $this->info['account'];
       $prop = $payment->addChild("prop");
         $prop->addAttribute("name", "cardnum");
         $prop->addAttribute("value", $acc);
-      
+
       $prop = $payment->addChild("prop");
         $prop->addAttribute("name", "country");
-        $prop->addAttribute("value", "UA");      
+        $prop->addAttribute("value", "UA");
     }
 
     $xml_inner_data = strip_tags($data->asXML(), "<oper><wait><test><payment><prop>");
     $signature = $this->merchant->calcSignature($xml_inner_data);
     $merchant->addChild("signature", $signature);
-    
+
     $xml_request = $request->asXML();
-    
-    
     $response = \Httpful\Request::post($this->balance_uri)->body($xml_request)->sendsXml()->expectsXml()->send();
-    
+
     $this->rawXml = $response->raw_body;
-    
+
     list( ,$info) = each($response->body->xpath('data/info'));
-    
+
     foreach($info->children() as $k=>$child)
     {
-	$name = $child->getName();
-	if ($name=="error")
-	{
-	    $this->status = self::STATUS_ERR;
-	    return false;
-	}
+    	$name = $child->getName();
+    	if ($name=="error")
+    	{
+    	    $this->status = self::STATUS_ERR;
+    	    return false;
+    	}
     }
 
     foreach ($response->body->data->info->cardbalance->children() as $key=>$value)
@@ -115,7 +113,7 @@ class Account
       }
       $this->balance[$key]=(string)$value;
     }
-    
+
     $this->status=self::STATUS_OK;
     return $this->balance;
   }
@@ -127,12 +125,12 @@ class Account
 
     return $this->info;
   }
-  
+
   public function xml()
   {
     if ($this->status==self::STATUS_NEW)
         $this->balance();
 
-    return $this->rawXml;  
+    return $this->rawXml;
   }
 }
